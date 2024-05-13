b5de5230cb462c3ab094c1aab96a0683eb635b87
박지원 <dwy0104@gmail.com>
2024-05-14 04:39:26 +0900

    coord/class output layer 분리

--- model.py
+++ model.py
@@ -1,13 +1,37 @@
 import torch
 import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader, TensorDataset
 from torchvision import models
 
 
 pretrained_resnet =  models.resnet34(pretrained = True)
 
+num_ftrs = pretrained_resnet.fc.in_features
+
 pretrained_resnet.fc = nn.Sequential(
-        nn.Linear(512, 128),
-        nn.ReLU(),
-        nn.Linear(128, 54), # 최총output: 54 (x1, y1, c1, ... , x18, y18, c18)
-        nn.Sigmoid() #좌표, classificaion 모두 0~1 사이 값이므로
+        nn.Linear(num_ftrs, 128), #layer 디자인 어떻게...??
+        nn.ReLU()
     )
+
+coordinate_head = nn.Linear(128, 34) #(x1, y1, ... , x17, y17)
+
+classification_head = nn.Sequential(
+    nn.Linear(128, 17), #(c1, ... , c17)
+    nn.Sigmoid()
+)
+
+
+class MultiHeadResNet34(nn.Module):
+    def __init__(self, pretrained_model, head1, head2):
+        super(MultiHeadResNet34, self).__init__()
+        self.pretrained_model = pretrained_model
+        self.head1 = head1
+        self.head2 = head2
+
+    def forward(self, x):
+        x = self.pretrained_model(x)
+        output1 = self.head1(x)
+        output2 = self.head2(x)
+        return output1, output2
+
+baseline_model = MultiHeadResNet34(pretrained_resnet, coordinate_head, classification_head)
--- train.py
+++ train.py
@@ -1,71 +1,39 @@
 import torch
 import torch.nn as nn
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader, TensorDataset
 from torchvision import models
 from model import *
 
 
 # 잘 작동되는지 보려고 샘플 트레이닝 데이터셋 만들어서 시험해봤음
-# data_size = (100, 3, 224, 224) 
-# label_size =(100, 54)
-# train_datas = torch.rand(data_size)
-# train_lables = torch.rand(label_size)
+# data_shape=(100,3,224,224)
+# train_inputs = torch.rand(data_shape)
+# coord_labels = torch.rand((100, 34))
+# class_labels = torch.randint(0, 2, (100, 17)).float()
 
-# class TestDataset(Dataset): 
-#   def __init__(self, train_inputs, train_lables):
-#     self.datas = train_inputs
-#     self.lables = train_lables
+# dataset = TensorDataset(train_inputs, coord_labels, class_labels)
 
-#   def __len__(self): 
-#     return len(self.datas)
+# batch_size = 20
+# input_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers = 4)
 
-#   def __getitem__(self, idx): 
-#     x = torch.FloatTensor(self.datas[idx])
-#     y = torch.FloatTensor(self.lables[idx])
-#     return x, y
+# criterion_coord = nn.MSELoss()
+# criterion_class = nn.BCELoss()
+# optim = torch.optim.Adam(baseline_model.parameters(), lr = 0.0001)
 
-# dataset = TestDataset(train_datas, train_lables)
-# batch_size = 20 
-# train_loader = DataLoader(dataset, batch_size = batch_size, shuffle = True, num_workers = 4)
-
-
-
-class MyLoss1(nn.Module):   
-    def __init__(self):
-        super(MyLoss1, self).__init__()
-
-    def forward(self, predictions, lables):
-        mse_loss = nn.MSELoss(reduction='mean')
-        loss = 0
-
-        predictions = predictions.view(-1, 18, 3) # output을 (batch_size, 18, 3) 형태로 재구성
-        lables = lables.view(-1, 18, 3)
-
-        for i in range(len(predictions)):
-            for j in range(18):
-                if predictions[i][j][2] > 0.5:  # classification score가 threshold를 넘는 경우
-                    loss += mse_loss(predictions[i][j], lables[i][j])  # 모든 요소에 대한 MSE를 loss에 더함
-                else:
-                    loss += mse_loss(predictions[i][j][2], lables[i][j][2])  # classification에 대한 MSE만 loss에 더함
-                
-        return loss / len(predictions)
-        
-
-criterion1 = MyLoss1()  # classification이 0.5 이하면(keypoint가 사진에 없어야 한다고 판단하면) x좌표, y좌표 값에대한 error loss에 반영 안함(안더함)
-criterion2 = nn.MSELoss() # keypoint가 없을 때 정답 label을 (0,0,0)으로 두는 경우 => predictin이 (0,0,0)이 되도록 학습하도록 MSE loss 사용
-optim = torch.optim.Adam(pretrained_resnet.parameters(), lr = 0.0001)
-
-# num_epoch = 10
-# for epoch in range(num_epoch):
-#   for i, data in enumerate(train_loader):
-#     inputs, labels = data
+# num_epochs = 5
+# for epoch in range(num_epochs):
+#   for inputs, coord_labels, class_labels in input_loader:
 #     optim.zero_grad()
-#     predictions = pretrained_resnet(inputs)
-#     loss = criterion1(predictions, labels)
-#     loss.backward()
+#     coord_outputs, class_outputs = baseline_model(inputs)
+
+#     coord_loss = criterion_coord(coord_outputs, coord_labels)
+#     class_loss = criterion_class(class_outputs, class_labels)
+#     total_loss = coord_loss + class_loss
+
+#     total_loss.backward()
 #     optim.step()
-  
-#   print("epoch: ", epoch, "loss: ", loss.item())
+
+#   print("epoch", epoch, "   Coord Loss:", coord_loss.item(), "  Class Loss:", class_loss.item())
 
 
-torch.save(pretrained_resnet.state_dict(), './baseline_resnet.pth')
+torch.save(baseline_model.state_dict(), './baseline_resnet.pth')
